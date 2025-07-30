58d2ecbf2478eeafbc68086f6a683c6f631d8fa5
Bit <bit@bitknows.tech>
2025-07-30 03:49:46 -0600

    Created tests for auth

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,69 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	type test struct {
+		name   string
+		input  http.Header
+		want   string
+		errMsg string
+	}
+
+	headerWithBadAuth := http.Header{}
+	headerWithBadAuth.Add("Authorization", "bad")
+
+	headerWithOKAuth := http.Header{}
+	headerWithOKAuth.Add("Authorization", "ApiKey TestKey")
+
+	tests := []test{
+		{
+			"No headers",
+			http.Header{},
+			"",
+			"no authorization header included",
+		},
+		{
+			"Bad Auth",
+			headerWithBadAuth,
+			"",
+			"malformed authorization header",
+		},
+		{
+			"ok",
+			headerWithOKAuth,
+			"TestKey",
+			"",
+		},
+		{
+			"ForceFail",
+			http.Header{},
+			"failure",
+			"",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := GetAPIKey(tc.input)
+			if tc.errMsg == "" {
+				if err != nil {
+					t.Errorf("Expected no error, got: %v", err)
+				}
+			} else {
+				if err == nil {
+					t.Errorf("Expected error: %q, got no error", tc.errMsg)
+				} else if err.Error() != tc.errMsg {
+					t.Errorf("Expected error message: %q, got %q", tc.errMsg, err.Error())
+				}
+			}
+
+			if got != tc.want {
+				t.Errorf("Expected: %q, got %q", tc.want, got)
+			}
+		})
+	}
+}
